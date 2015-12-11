1250e15b1b6a675fd4fbf8b281e441426bfc30d5
yql776476966 <776476966@qq.com>
2015-12-11 14:52:21 +0800

    发货方订单详情

--- Edyd/app/src/main/java/com/oto/edyd/utils/NumberFormat.java
+++ Edyd/app/src/main/java/com/oto/edyd/utils/NumberFormat.java
@@ -9,7 +9,7 @@ public class NumberFormat {
 
     private String INPUT_DOUBLE_NUMBER = "00";
     /**
-     * 格式化两位数字，如果是1~9怎数字前面补零
+     * 格式化两位数字，如果是1~9则数字前面补零
      * @param obj
      * @return
      */
