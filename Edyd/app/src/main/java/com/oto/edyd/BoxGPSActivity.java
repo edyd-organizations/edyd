e1ab53d39be05f35bab00b2ceb7255d497f04a7f
e <506886298@qq.com>
2015-12-18 16:16:45 +0800

    1.增加了高德导航的jar包和语音类； 2.修改了轨迹的自定义显示；

---
+++ Edyd/app/src/main/java/com/oto/edyd/BoxGPSActivity.java
@@ -0,0 +1,15 @@
+package com.oto.edyd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by liubaozhong on 2015/12/18.
+ * 百宝箱导航
+ */
+public class BoxGPSActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
