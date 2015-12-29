0e454932bbf9298556e7fd75d6038af998f691c3
e <506886298@qq.com>
2015-12-29 09:11:44 +0800

    1.解决了进程加载动画停留。无法消失的bug；

--- Edyd/app/src/main/java/com/oto/edyd/utils/CusProgressDialog.java
+++ Edyd/app/src/main/java/com/oto/edyd/utils/CusProgressDialog.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -29,13 +30,15 @@ public class CusProgressDialog {
         View v = inflater.inflate(R.layout.loading_dialog, null); //得到加载view
         LinearLayout layout = (LinearLayout) v.findViewById(R.id.dialog_view); //加载布局
         ImageView loadingDataImage = (ImageView) v.findViewById(R.id.img);
-        TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);
+       // TextView tipTextView = (TextView) v.findViewById(R.id.tipTextView);
         //加载动画
         Animation loadingDataAnimation = AnimationUtils.loadAnimation(context, R.anim.loading_animation); //加载动画
+        LinearInterpolator lin = new LinearInterpolator();
+        loadingDataAnimation.setInterpolator(lin);
         loadingDataImage.startAnimation(loadingDataAnimation); //让ImagView显示动画
-        tipTextView.setText(msg); //设置加载信息
+        //tipTextView.setText(msg); //设置加载信息
         Dialog loadingDialog = new Dialog(context, R.style.LoadingDialog); //创建自定义样式
-        loadingDialog.setCancelable(false); //不可以用“返回键”取消
+        loadingDialog.setCancelable(true); //不可以用“返回键”取消
         loadingDialog.setContentView(layout, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT)); //设置布局
         return loadingDialog;
     }
