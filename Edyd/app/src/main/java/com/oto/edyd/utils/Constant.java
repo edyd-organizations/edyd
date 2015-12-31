d6284cab2b18980cb0c6a568ece35d8698fb9624
yql776476966 <776476966@qq.com>
2015-12-31 17:36:56 +0800

    1、修改为测试地址

--- Edyd/app/src/main/java/com/oto/edyd/utils/Constant.java
+++ Edyd/app/src/main/java/com/oto/edyd/utils/Constant.java
@@ -29,14 +29,14 @@ public class Constant {
     //public final static String ACCOUNT_ID = "account_id";
 
     //网络地址
-   // public final static String ENTRANCE_PREFIX = "http://120.24.236.223/api/v1.0/"; //接口前缀地址
-    public final static String ENTRANCE_PREFIX = "http://www.edyd.cn/api/v1.0/"; //接口前缀地址
+    public final static String ENTRANCE_PREFIX = "http://120.24.236.223/api/v1.0/"; //接口前缀地址
+   // public final static String ENTRANCE_PREFIX = "http://www.edyd.cn/api/v1.0/"; //接口前缀地址
 
-   // public final static String ENTRANCE_PREFIX_v1 = "http://120.24.236.223/api/v1.1/"; //接口前缀地址
-    public final static String ENTRANCE_PREFIX_v1 = "http://www.edyd.cn/api/v1.1/"; //接口前缀地址
+    public final static String ENTRANCE_PREFIX_v1 = "http://120.24.236.223/api/v1.1/"; //接口前缀地址
+   // public final static String ENTRANCE_PREFIX_v1 = "http://www.edyd.cn/api/v1.1/"; //接口前缀地址
 
-   // public final static String CMB_CALLBACK_ADDRESS = "http://120.24.236.223/callback/updateBillStatus.json";
-    public final static String CMB_CALLBACK_ADDRESS = "http://www.edyd.cn/callback/updateBillStatus.json";
+    public final static String CMB_CALLBACK_ADDRESS = "http://120.24.236.223/callback/updateBillStatus.json";
+   // public final static String CMB_CALLBACK_ADDRESS = "http://www.edyd.cn/callback/updateBillStatus.json";
 
     public final static String EDYD = "http://www.edyd.cn/";
     //系统状态码
